1deb05512c873b9e9d94258a70d7c51fce208b61
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-07-05 11:49:13 +0530

    update

--- 20-sg/parameters.tf
+++ 20-sg/parameters.tf
@@ -45,3 +45,10 @@ resource "aws_ssm_parameter" "web_alb_sg_id" {
   type  = "String"
   value = module.web_alb_sg.id
 }
+
+resource "aws_ssm_parameter" "ingress_alb_sg_id" {
+  # /expense/dev/mysql_sg_id
+  name  = "/${var.project_name}/${var.environment}/ingress_alb_sg_id"
+  type  = "String"
+  value = module.ingress_alb_sg.id
+}
