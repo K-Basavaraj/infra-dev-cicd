65f276763178bf1d7f81a2b3902cffdfa80e0f3c
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-07-05 12:42:12 +0530

    update

--- 50-eks/locals.tf
+++ 50-eks/locals.tf
@@ -1,5 +1,5 @@
 locals {
   private_subnet_ids  = split(",", data.aws_ssm_parameter.private_subnet_ids.value) #convert stringlist to list and select particular subnet
-  control_plane_sg_id = data.aws_ssm_parameter.control_plane_sg_id.value
+  control_plane_sg_id = data.aws_ssm_parameter.eks_control_plane_sg_id.value
   node_sg_id          = data.aws_ssm_parameter.node_sg_id.value
 }
