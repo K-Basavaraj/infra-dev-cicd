4e81277656abee45eee8c5d94831a3198b6f09e5
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-07-05 12:32:43 +0530

    update

--- 50-eks/data.tf
+++ 50-eks/data.tf
@@ -7,7 +7,7 @@ data "aws_ssm_parameter" "vpc_id" {
 }
 
 data "aws_ssm_parameter" "eks_control_plane_sg_id" {
-  name = "/${var.project_name}/${var.environment}eks_control_plane_sg_id"
+  name = "/${var.project_name}/${var.environment}/eks_control_plane_sg_id"
 }
 
 data "aws_ssm_parameter" "node_sg_id" {
--- 80-alb/data.tf
+++ 80-alb/data.tf
@@ -6,8 +6,8 @@ data "aws_ssm_parameter" "public_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/public_subnet_ids"
 } 
 
-data "aws_ssm_parameter" "ingress_alb_sg" {
-  name = "/${var.project_name}/${var.environment}/ingress_alb_sg"
+data "aws_ssm_parameter" "ingress_alb_sg_id" {
+  name = "/${var.project_name}/${var.environment}/ingress_alb_sg_id"
 }
 
 data "aws_ssm_parameter" "https_certificate_arn" {
