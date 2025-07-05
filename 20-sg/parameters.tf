46b43067981029cdfb3066027170b1c624095ce6
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-07-05 12:23:24 +0530

    update

--- 20-sg/parameters.tf
+++ 20-sg/parameters.tf
@@ -52,3 +52,17 @@ resource "aws_ssm_parameter" "ingress_alb_sg_id" {
   type  = "String"
   value = module.ingress_alb_sg.id
 }
+
+resource "aws_ssm_parameter" "eks_control_plane_sg_id" {
+  # /expense/dev/mysql_sg_id
+  name  = "/${var.project_name}/${var.environment}/eks_control_plane_sg_id"
+  type  = "String"
+  value = module.eks_control_plane_sg.id
+}
+
+resource "aws_ssm_parameter" "node_sg_id" {
+  # /expense/dev/mysql_sg_id
+  name  = "/${var.project_name}/${var.environment}/node_sg_id"
+  type  = "String"
+  value = module.node_sg.id
+}
